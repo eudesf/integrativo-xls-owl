0d9cbd36e83f1bf668a4f876b8cdbbe3ea411ea6
Eudes <aeudes@gmail.com>
2016-03-18 01:22:34 -0300

    owl funcional em teste

--- integrativo-xls-owl/src/main/java/Main.java
+++ integrativo-xls-owl/src/main/java/Main.java
@@ -1,13 +1,17 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -46,63 +50,83 @@ public class Main {
 			throw new FileNotFoundException(args[0]);
 		}
 		
-		FileWriter writer = new FileWriter("output.owl");
-		writer.append(new Main().convert(file));
+		BufferedWriter writer = new BufferedWriter(new FileWriter("output.owl"), 1024*1024*50);
+		new Main().convert(file, writer);
 		writer.close();
 	}
 
 
-	private String convert(File file) throws IOException {
+	private void convert(File file, Writer writer) throws IOException {
 		InputStream in = new FileInputStream(file);
 		Workbook wb = new XSSFWorkbook(in);
-		StringBuilder out = new StringBuilder();
 		
-		Sheet elementsSheet = wb.getSheet("owl-elements2");
-		for (int elementRowIndex = 1; elementRowIndex < 27; elementRowIndex++) {
-			Row elementRow = elementsSheet.getRow(elementRowIndex);
-			out.append(columnsJoinText(elementRow));
+		Sheet modelsSheet = wb.getSheet("owl-elements2");
+		for (int modelRowIndex = 1; modelRowIndex < 27; modelRowIndex++) {
+			Row modelRow = modelsSheet.getRow(modelRowIndex);
+			writer.append(columnsJoinText(rowToList(modelRow)));
 		}
 
 		Sheet dataSheet = wb.getSheet("final-data");
 		for (int dataRowIndex = 1; dataRowIndex < 48; dataRowIndex++) {
 			Row dataRow = dataSheet.getRow(dataRowIndex);
-			String[] dataRowArray = convertToDataRowArray(dataRow);
 			
-//			for (int elementRowIndex = 27; elementRowIndex < 52; elementRowIndex++) {
-//				Row elementRow = elementsSheet.getRow(elementRowIndex);
-//				out.append(expandData(columnsJoinText(elementRow), dataRow));
-//			}
+			List<String> dataRowList = rowToList(dataRow);
+			writer.append("\n    " + strComm("XLS line: " + dataRowList.toString()) + "\n");
+			writer.append("   " + strComm("=========") + "\n");
+			
+			System.out.println("\n\n***** " + dataRowList.toString() + "\nCalculating rows...");
+			Set<List<String>> rowRows = getRowRows(dataRowList);
+			System.out.println("Writing rows...");
+			int rowNum = 1;
+			for (List<String> rowRow : rowRows) {
+				writer.append("    " + strComm("Derived line (" + rowNum + "/" + rowRows.size() + "): " + rowRow.toString()) + "\n");
+				for (int modelRowIndex = 27; modelRowIndex < 52; modelRowIndex++) {
+					List<String> modelRow = rowToList(modelsSheet.getRow(modelRowIndex));
+					writer.append(expandData(columnsJoinText(modelRow), rowRow));
+				}
+				System.out.print("\r" + rowNum + "/" + rowRows.size());
+				rowNum++;
+			}
+			break;
 		}
 		
-		Row lastElementRow = elementsSheet.getRow(elementsSheet.getLastRowNum());
-		out.append(columnsJoinText(lastElementRow));
+		Row lastElementRow = modelsSheet.getRow(modelsSheet.getLastRowNum());
+		
+		writer.append(columnsJoinText(rowToList(lastElementRow)));
 		
 		wb.close();
-		
-		return out.toString();
-	}
-
-	private List<String[]> listDataRowArray(Row dataRow) {
-		String[][] dataRowArray = new String[12][];
-		for (int column = 0; column < 12; column++) {
-			String cellText = dataRow.getCell(column).getStringCellValue();
-			dataRowArray[column] = cellText.split(";");
-		}
-		List<String[]> list = new ArrayList<>();
-		appendDataRowArrayToList(dataRowArray, list);
-		return list;
-	}
-
-	private void appendDataRowArrayToList(String[][] dataRowArray, List<String[]> list) {
-		
 	}
 	
-	private String columnsJoinText(Row elementRow) {
+	private List<String> rowToList(Row row) {
+		List<String> rowList = new ArrayList<>();
+		for (int column = 0; column < row.getLastCellNum(); column++) {
+			rowList.add(row.getCell(column).getStringCellValue());
+		}
+		return rowList;
+	}
+
+	private Set<List<String>> getRowRows(List<String> row) {
+		Set<List<String>> rowRows = new HashSet<>();
+		for (int col = 0; col < row.size(); col++) {
+			String colText = row.get(col);
+			String[] elements = colText.split(";");
+			if (elements.length > 1) {
+				List<String> newRow = new ArrayList<>(row);
+				newRow.set(col, colText.substring(colText.indexOf(";") + 1));
+				rowRows.addAll(getRowRows(newRow));
+				row.set(col, colText.substring(0, colText.indexOf(";")));
+			}
+		}
+		rowRows.add(row);
+		return rowRows;
+	}
+
+	private String columnsJoinText(List<String> modelRow) {
 		StringBuilder out = new StringBuilder();
 //		out.append("\n");
 //		out.append(strComm(elementRow.getCell(0).getStringCellValue()));
 //		out.append("\n");
-		out.append(elementRow.getCell(1).getStringCellValue());
+		out.append(modelRow.get(1));
 		out.append("\n");
 		return out.toString();
 	}
@@ -111,17 +135,17 @@ public class Main {
 		return "$" + column + "$";
 	}
 	
-	private String expandData(String elementText, Row dataRow) {
+	private String expandData(String elementText, List<String> dataRow) {
 		for (String column : columnMap.keySet()) {
 			String columnKey = formatColumnKey(column);
 			if (elementText.contains(columnKey)) {
-				elementText = elementText.replace(columnKey, dataRow.getCell(columnMap.get(column)).getStringCellValue());
+				elementText = elementText.replace(columnKey, strNormalizedName(dataRow.get(columnMap.get(column))));
 			}
 		}
 		for (String predefinedColumn : predefinedColumnMap.keySet()) {
 			String predefinedColumnKey = formatColumnKey(predefinedColumn);
 			if (elementText.contains(predefinedColumnKey)) {
-				elementText = elementText.replace(predefinedColumnKey, predefinedColumnMap.get(predefinedColumn));
+				elementText = elementText.replace(predefinedColumnKey, strNormalizedName(predefinedColumnMap.get(predefinedColumn)));
 			}
 		}
 
